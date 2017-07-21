546ae59f136d90ecabe5e497e0e469ae545950d6
Benjamin <benj@zagorski.com>
2017-07-21 15:44:05 -0700

    Cleaned up the TextMessagesController

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,21 +1,11 @@
 class TextMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    # send_text_message("+7148099426", "Yo")
-
-    # put your own credentials here
-
     account_sid = 'AC2f849325773a8d8bb652a418f772ea3c'
     auth_token = 'b6ad929d59deb307545ecd808d59f3f8'
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
-
-    p "========================================="
-    p @client
-    p "========================================="
-    p @client.accounts
-
 
     @client.messages.create({
       :from => '+15624554829',
@@ -23,7 +13,7 @@ class TextMessagesController < ApplicationController
       :body => 'Yo',
       # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
     })
-    render json: "Yo"
+    render json: "Text message sent to 17148099426"
   end
 
   def index
