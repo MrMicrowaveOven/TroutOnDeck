0d824b1cdd183205c845ad6601f3ac3f2ad8a364
Benjamin <benj@zagorski.com>
2017-07-21 16:56:20 -0700

    Controller now sends text messages, and button now calls a POST request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :text_messages
+  # resources :text_messages
+  get "/text_messages/new" => "text_messages#new"
+  # get "/text_messages" => "text_messages#index"
+  post "/text_messages" => "text_messages#create"
+  # delete "/text_messages" => "text_messages#destroy"
+  root 'text_messages#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
