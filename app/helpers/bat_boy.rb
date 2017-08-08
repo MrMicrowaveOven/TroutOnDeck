daf17419ca7de9de12ec89cb1af41f463c03177b
Benjamin <benj@zagorski.com>
2017-08-08 14:15:46 -0700

    Moved BatBoy and Commentator to their own helper docs

---
+++ app/helpers/bat_boy.rb
@@ -0,0 +1,11 @@
+require 'open-uri'
+require 'json'
+
+class BatBoy
+  def self.game_going_on?(game_id)
+    response = open('https://api.sportradar.us/mlb-t6/games/' + game_id + '/pbp.json?api_key=' + ENV['SPORTRADAR_KEY']).read
+    gameStatus = (JSON.parse(response))["game"]["status"]
+
+    gameStatus == "inprogress"
+  end
+end
