f8ab7c99a1b392ba0c4c69ce0b41685c5f66ce8f
Benjamin <benj@zagorski.com>
2017-08-08 01:50:48 -0700

    Controller now makes call to SportRadar API to ensure game is still going before sending text message

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,31 +1,43 @@
+require 'open-uri'
+require 'json'
 class TextMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    account_sid = ENV['TWILLIO_SID']
-    auth_token = ENV['TWILLIO_TOKEN']
+    gameId = params["gameId"]
+    response = open('https://api.sportradar.us/mlb-t6/games/' + gameId + '/pbp.json?api_key=' + ENV['SPORTRADAR_KEY']).read
+    gameStatus = (JSON.parse(response))["game"]["status"]
 
-    # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    jsonResponse = {}
+    if gameStatus != "inprogress"
+      jsonResponse["gameInProgress"] = false
+    else
+      jsonResponse["gameInProgress"] = true
+      account_sid = ENV['TWILLIO_SID']
+      auth_token = ENV['TWILLIO_TOKEN']
 
-    @client.messages.create({
-      :from => '+15624554829',
-      :to => '+17148099426',
-      :body => 'Trout is on deck!',
-      # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-    })
-    @client.messages.create({
-      :from => '+15624554829',
-      :to => '+17144868433',
-      :body => 'Trout is on deck!',
-      # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-    })
-    @client.messages.create({
-      :from => '+15624554829',
-      :to => '+17148156295',
-      :body => 'Trout is on deck!',
-      # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-    })
-    render json: {success: "Text message sent to 17148099426"}
+      # set up a client to talk to the Twilio REST API
+      @client = Twilio::REST::Client.new account_sid, auth_token
+
+      @client.messages.create({
+        :from => '+15624554829',
+        :to => '+17148099426',
+        :body => 'Trout is on deck!',
+        # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
+      })
+      @client.messages.create({
+        :from => '+15624554829',
+        :to => '+17144868433',
+        :body => 'Trout is on deck!',
+        # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
+      })
+      @client.messages.create({
+        :from => '+15624554829',
+        :to => '+17148156295',
+        :body => 'Trout is on deck!',
+        # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
+      })
+    end
+    render json: jsonResponse
   end
 
   def new
