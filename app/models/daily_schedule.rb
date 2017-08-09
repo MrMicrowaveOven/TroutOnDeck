debb26a5b4b06dbe7a78d6c91e0934ee700e372b
Benjamin <benj@zagorski.com>
2017-08-09 13:42:21 -0700

    Schedule checker now relies on daily updated database

--- app/models/daily_schedule.rb
+++ app/models/daily_schedule.rb
@@ -1,9 +1,14 @@
 require 'open-uri'
 require 'json'
+require 'date'
 
 class DailySchedule < ApplicationRecord
   def self.update_schedule
-    response = open('https://api.sportradar.us/mlb-t6/games/2017/8/8/schedule.json?api_key=' + ENV['SPORTRADAR_KEY']).read
+    current_date = Date.today
+    current_year = current_date.year
+    current_month = current_date.month
+    current_day = current_date.day
+    response = open('https://api.sportradar.us/mlb-t6/games/' + current_year.to_s + '/' + current_month.to_s + '/' + current_day.to_s + '/schedule.json?api_key=' + ENV['SPORTRADAR_KEY']).read
     games = (JSON.parse(response))["games"]
     angelsGames = games.select {|game| game["home_team"] == "4f735188-37c8-473d-ae32-1f7e34ccf892" || game["away_team"] == "4f735188-37c8-473d-ae32-1f7e34ccf892"}
     angelsGames.each do |angelGame|
