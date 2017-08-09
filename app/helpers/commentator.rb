f9e5dfdb02cc3715c81ad783a689cc2c5838672d
Benjamin <benj@zagorski.com>
2017-08-09 13:59:17 -0700

    Refactored Commentator so phone nmbers are in array form

--- app/helpers/commentator.rb
+++ app/helpers/commentator.rb
@@ -20,23 +20,19 @@ class Commentator
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    @client.messages.create({
-      :from => '+15624554829',
-      :to => '+17148099426',
-      :body => 'Trout is on deck!',
-      # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-    })
-    @client.messages.create({
-      :from => '+15624554829',
-      :to => '+17144868433',
-      :body => 'Trout is on deck!',
-      # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-    })
-    @client.messages.create({
-      :from => '+15624554829',
-      :to => '+17148156295',
-      :body => 'Trout is on deck!',
-      # :media_url => 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
-    })
+    phone_numbers = [
+      '+17148099426',
+      '+17144868433',
+      '+17148156295'
+    ]
+
+    phone_numbers.each do |phone_number|
+      @client.messages.create({
+        :from => '+15624554829',
+        :to => phone_number,
+        :body => 'Trout is on deck!',
+      })
+    end
+
   end
 end
