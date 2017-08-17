af56b255e5b5db3e84ec632804758f03d7056db0
Benjamin <benj@zagorski.com>
2017-08-17 01:43:47 -0700

    Refactored setups into seperate commits

--- test/helpers/bat_boy_helper_test.rb
+++ test/helpers/bat_boy_helper_test.rb
@@ -2,35 +2,39 @@ require 'test_helper'
 require_relative 'bat_boy_helper_test_api_returns/home_game_over'
 
 class BatBoyHelperTest < ActionView::TestCase
+  def setup
+    BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
+    @batboy_for_home_game_over = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
+  end
   test "should stub call to API properly" do
     BatBoy.any_instance.stubs(:get_game_info).returns({very: "sexy"})
-
     assert_equal ({very: "sexy"}), BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c").inspect
   end
   test "should use stubbed version of game JSON" do
-    BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
-
-    batboy = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
-    assert_equal home_game_over, batboy.inspect
+    assert_equal home_game_over, @batboy_for_home_game_over.inspect
   end
 
   class BatBoy_GameGoingOnTest < ActionView::TestCase
+    def setup
+      BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
+      @batboy_for_home_game_over = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
+    end
     test "returns true if the game is still on" do
 
     end
 
     test "returns false if the game is over" do
-      BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
-      batboy = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
-      assert_equal false, batboy.game_going_on?
+      assert_equal false, @batboy_for_home_game_over.game_going_on?
     end
   end
 
   class BatBoy_AngelsHomeGameTest < ActionView::TestCase
-    test "returns true if venue is Angel's stadium" do
+    def setup
       BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
-      batboy = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
-      assert_equal true, batboy.angels_home_game?
+      @batboy_for_home_game_over = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
+    end
+    test "returns true if venue is Angel's stadium" do
+      assert_equal true, @batboy_for_home_game_over.angels_home_game?
     end
 
     test "returns false if venue is not Angel's stadium" do
@@ -40,10 +44,12 @@ class BatBoyHelperTest < ActionView::TestCase
 
 
   class BatBoy_PlayerAtBatTest < ActionView::TestCase
-    test "should return nil if game is over" do
+    def setup
       BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
-      batboy = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
-      assert_nil batboy.player_at_bat
+      @batboy_for_home_game_over = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
+    end
+    test "should return nil if game is over" do
+      assert_nil @batboy_for_home_game_over.player_at_bat
     end
 
     test "should return player id if a player is at bat" do
@@ -52,10 +58,12 @@ class BatBoyHelperTest < ActionView::TestCase
   end
 
   class BatBoy_TroutAtBatTest < ActionView::TestCase
-    test "should return false if game is over" do
+    def setup
       BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
-      batboy = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
-      assert_equal false, batboy.trout_at_bat?
+      @batboy_for_home_game_over = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
+    end
+    test "should return false if game is over" do
+      assert_equal false, @batboy_for_home_game_over.trout_at_bat?
     end
 
     test "should return false if another player is at bat" do
@@ -68,6 +76,10 @@ class BatBoyHelperTest < ActionView::TestCase
   end
 
   class BatBoy_TroutOnDeckTest < ActionView::TestCase
+    def setup
+      BatBoy.any_instance.stubs(:get_game_info).returns(home_game_over)
+      @batboy_for_home_game_over = BatBoy.new("d7a9a7c5-958b-453b-8789-5bc9469a8e7c")
+    end
     test "returns false if Trout is not on deck" do
 
     end
