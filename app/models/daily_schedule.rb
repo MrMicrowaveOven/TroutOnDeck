f2b2df1ab4ceebc244bcca1dbd342192806a688a
Benj <benj@rocksbox.com>
2018-05-12 16:37:31 -0700

    Added /en/ to api call, because fuck off

--- app/models/daily_schedule.rb
+++ app/models/daily_schedule.rb
@@ -9,7 +9,8 @@ class DailySchedule < ApplicationRecord
     current_year = current_date.year
     current_month = current_date.month
     current_day = current_date.day
-    response = open('https://api.sportradar.us/mlb-t6/games/' + current_year.to_s + '/' + current_month.to_s + '/' + current_day.to_s + '/schedule.json?api_key=' + ENV['SPORTRADAR_KEY']).read
+    response = open('https://api.sportradar.us/mlb-t6/en/games/' + current_year.to_s + '/' + current_month.to_s + '/' + current_day.to_s + '/schedule.json?api_key=' + ENV['SPORTRADAR_KEY']).read
+
     games = (JSON.parse(response))["games"]
     angelsGames = games.select {|game| game["home_team"] == "4f735188-37c8-473d-ae32-1f7e34ccf892" || game["away_team"] == "4f735188-37c8-473d-ae32-1f7e34ccf892"}
     angelsGames.each do |angelGame|
