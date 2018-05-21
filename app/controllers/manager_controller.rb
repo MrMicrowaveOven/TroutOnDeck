afa704cf2849040a484020b7836b7980f87ac644
Benj <benj@rocksbox.com>
2018-05-21 01:32:15 -0400

    Moved logic to ManagerHelper

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -6,26 +6,8 @@ class ManagerController < ApplicationController
   include ManagerHelper
 
   def create
-    game_id = params[:gameId]
+    jsonResponse = ManagerHelper.send_text_if_valid(params[:gameId])
 
-
-    batboy = BatBoy.new(game_id)
-    gameInProgress = batboy.game_going_on?
-
-    jsonResponse = {
-      gameInProgress: gameInProgress,
-      textSentRecently: Alert.text_sent_recently?,
-      troutAtBat: batboy.trout_at_bat?,
-      troutOnDeck: batboy.trout_on_deck?
-    }
-
-    if gameInProgress && !jsonResponse[:textSentRecently]
-      if jsonResponse[:troutAtBat] || jsonResponse[:troutOnDeck]
-        marked_time = Alert.new
-        marked_time.save!
-        Commentator.send_texts
-      end
-    end
     render json: jsonResponse
   end
 end
--- app/helpers/manager_helper.rb
+++ app/helpers/manager_helper.rb
@@ -1,4 +1,25 @@
 module ManagerHelper
   include BatBoyHelper
   include CommentatorHelper
+
+  def self.send_text_if_valid(game_id)
+    batboy = BatBoy.new(game_id)
+    gameInProgress = batboy.game_going_on?
+
+    jsonResponse = {
+      gameInProgress: gameInProgress,
+      textSentRecently: Alert.text_sent_recently?,
+      troutAtBat: batboy.trout_at_bat?,
+      troutOnDeck: batboy.trout_on_deck?
+    }
+
+    if gameInProgress && !jsonResponse[:textSentRecently]
+      if jsonResponse[:troutAtBat] || jsonResponse[:troutOnDeck]
+        marked_time = Alert.new
+        marked_time.save!
+        Commentator.send_texts
+      end
+    end
+    jsonResponse
+  end
 end
