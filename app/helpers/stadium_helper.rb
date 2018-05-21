3e8a6b02a6885215962eea67f87b12c7b70094d4
Benj <benj@rocksbox.com>
2018-05-21 02:05:33 -0400

    Added a buffer so the Stadium doesn't break prematurely

--- app/helpers/stadium_helper.rb
+++ app/helpers/stadium_helper.rb
@@ -4,9 +4,9 @@ module StadiumHelper
   class Stadium
     def initialize(game_time, game_id)
       sleep(game_time - Time.now)
-      while true
+      200.times do |index|
         at_bat_info = ManagerHelper.send_text_if_valid(game_id)
-        break unless at_bat_info[:gameInProgress]
+        break if at_bat_info[:gameInProgress] && index > 10
         if at_bat_info[:troutOnDeck] || at_bat_info[:troutAtBat]
           sleep 15
         else
