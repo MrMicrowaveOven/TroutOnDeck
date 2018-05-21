482636d0d9c2628da946edac02e8f9954784d76c
Benj <benj@rocksbox.com>
2018-05-21 02:07:42 -0400

    Added basic logs

--- app/helpers/stadium_helper.rb
+++ app/helpers/stadium_helper.rb
@@ -3,7 +3,9 @@ module StadiumHelper
 
   class Stadium
     def initialize(game_time, game_id)
+      p "SLEEP BEGINNING"
       sleep(game_time - Time.now)
+      p "GAMEON!!!"
       200.times do |index|
         at_bat_info = ManagerHelper.send_text_if_valid(game_id)
         break if at_bat_info[:gameInProgress] && index > 10
